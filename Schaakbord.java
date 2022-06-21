121e96b3607aad1f02c9aae594f3a86ae61dea12
paulmoonen <paul_moonen@hotmail.com>
2022-06-21 22:26:24 +0200

    click events with coordinates

--- Schaakbord.java
+++ Schaakbord.java
@@ -1,6 +1,8 @@
 package schaakbord;
-//import java.awt.*;
+import java.awt.event.*;
+//import java.awt.MouseInfo.*;
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
 
 public class Schaakbord {
     public static void main(String args[]){
@@ -14,7 +16,35 @@ public class Schaakbord {
         ButtonView btnvw = new ButtonView(bordData, schaakbordview);
         TextView tvw = new TextView();
         ApplicationView appview = new ApplicationView(tvw, schaakbordview, btnvw);
-        
+
+        schaakbordview.addMouseListener(new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent e){
+                int x = e.getX();
+                int y = e.getY();
+                System.out.println("klik, x: " + x + " y: " + y);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e){
+                System.out.println("mouse exited");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e){
+                System.out.println("mouse entered");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e){
+                System.out.println("mouse pressed");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e){
+                System.out.println("mouse released");
+            }
+        });                  
         
         myFrame.setSize(400, 400);
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
